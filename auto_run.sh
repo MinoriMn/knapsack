cfaf33057215b2f294bd7ce17d388a1cae46076c
MinoriMn <lucky_bluebrowneye10280915@au.com>
2020-03-22 20:33:01 +0900

    オートループ実行搭載

--- auto_run.sh
+++ auto_run.sh
@@ -1,4 +1,6 @@
-./knapsack
-mv ga_result_raw_record.csv $1/ga_result_raw_record.csv
-mv ga_result_uni_record.csv $1/ga_result_uni_record.csv
-mv ga_result_graph.pdf $1/ga_result_graph.pdf
+for ((i=0; i < $1; i++)); do
+  ./knapsack
+  mv ga_result_raw_record.csv $2/ga_result_raw_record_${i}.csv
+  mv ga_result_uni_record.csv $2/ga_result_uni_record_${i}.csv
+  mv ga_result_graph.pdf $2/ga_result_graph_${i}.pdf
+done
